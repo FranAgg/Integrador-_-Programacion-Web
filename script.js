75e64d14fa0274ae5ea62630735673910f1bd97f
Rooomeo <caruso.tete@gmail.com>
2024-10-24 15:52:39 -0300

    script prueba 2

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 const carouselImages = [
-    "img/latas_png20.png",
-    "img/papapapncho20.png",
-    "img/pipiiippipipiipiipipipzza20.png"
+    { src: "img/pipiiippipipiipiipipipzza20.png", link: "productos_piz.html" }, // Bebidas
+    { src: "img/papapapncho20.png", link: "productos_pan.html" }, // Panchos
+    { src: "img/latas_png20.png", link: "productos_beb.html" } // Pizzas
 ];
 
 let currentImageIndex = 0;
@@ -10,9 +10,8 @@ const carouselImage = document.getElementById("carrusel-imagenes");
 const prevBtn = document.getElementById("btn-izq");
 const nextBtn = document.getElementById("btn-der");
 
-
 function showImage(index) {
-    carouselImage.src = carouselImages[index];
+    carouselImage.src = carouselImages[index].src; 
 }
 
 
@@ -26,3 +25,10 @@ nextBtn.addEventListener("click", () => {
     currentImageIndex = (currentImageIndex + 1) % carouselImages.length;
     showImage(currentImageIndex);
 });
+
+
+carouselImage.addEventListener("click", () => {
+    window.location.href = carouselImages[currentImageIndex].link; 
+});
+
+showImage(currentImageIndex);
